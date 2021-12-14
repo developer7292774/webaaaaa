e5dc07031c8261143e70bd56b5b44b4d90377294
developer7292774 <94847369+developer7292774@users.noreply.github.com>
2021-12-14 15:24:36 +0600

    Update start.py

--- start.py
+++ start.py
@@ -3,4 +3,4 @@ import os
 os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.tgz")
 os.system("tar -xzvf ngrok-stable-linux-386.tgz")
 os.system("./ngrok authtoken 21C37ILhHmrN3LpkxWNBZ3p9TTq_4XquCTmDafA1ko2ETvKYR")
-os.system("./ngrok http file:////")
+os.system("./ngrok http file:////app/site")
